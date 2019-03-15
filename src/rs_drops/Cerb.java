9e57832de6c2eb6795664caca033f0f7ffb446a7
phil-ibm <phil@ibm.com>
2019-03-15 09:35:07 -0500

    added cerberus

--- src/rs_drops/Cerb.java
+++ src/rs_drops/Cerb.java
@@ -4,46 +4,30 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-public class Corp {
-    public static void main(String[] args){
+public class Cerb {
+	public static void main(String[] args){
 
         Long startTime = System.currentTimeMillis();
         int counter = 0;
         int min = 1000000;
         int max = 0;
-        int accounts = 1000000;
+        int accounts = 100000;
         Long sumdrops = 0L;
         int randnum;
         int uniques[] = {
-                0, //ely
-                0, // spectral
-                0, //arcane
-                0, //blessed
-                0, //onyx
-                0   //holy
+                0,
+                0,
+                0
                 };
         Random rand = new Random();
         for (int i = 0; i < accounts; i++) {
             while (IntStream.of(uniques).anyMatch(x -> x == 0)) {
-                randnum = rand.nextInt(585);
-                if (randnum == 584) {
-                    randnum = rand.nextInt(7);
-                    if(randnum==1){
-                        uniques[0]+=1;
+                randnum = rand.nextInt(128);
+                if(randnum==127){
+                    randnum= rand.nextInt(4);
+                        if(randnum>=0 &&randnum<3 ){
+                            uniques[randnum]+=1;
                     }
-                    if(randnum>=2 && randnum <=4){
-                        uniques[1]+=1;
-                    }
-                    if(randnum>=5){
-                        uniques[2]+=1;
-                    }
-
-                }else if(randnum >=0 && randnum <10){
-                    uniques[3]+=1;
-                }else if(randnum <= 33 && randnum >=10){
-                    uniques[4]+=1;
-                }else if(randnum>33 && randnum <=36){
-                    uniques[5]+=1;
                 }
 
                 counter++;
